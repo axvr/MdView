0aab6d92d7e86b5b7a2ab67fef1858d4c9f4bd84
Alex Vear <av@axvr.io>
2020-02-03 16:55:05 +0000

    Add `Null` template to disable rendering of Markdown elements

---
+++ Axvr.Xamarin.Markdown/Templates/Null.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+
+namespace Axvr.Xamarin.Markdown.Templates
+{
+    /// <summary>
+    /// Null Markdown template. Used to disable rendering of specific Markdown elements.
+    /// </summary>
+    public sealed class Null : View
+    { }
+}
