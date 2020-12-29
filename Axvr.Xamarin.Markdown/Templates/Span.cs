66cbaf5847c74676299a5280fae116773da9fae6
Alex Vear <av@axvr.io>
2020-12-29 13:51:46 +0000

    Add new properties to customise the default span template

--- Axvr.Xamarin.Markdown/Templates/Span.cs
+++ Axvr.Xamarin.Markdown/Templates/Span.cs
@@ -24,12 +24,8 @@ namespace Axvr.Xamarin.Markdown.Templates
 
     public class Span : global::Xamarin.Forms.Span
     {
-        // TODO: make this default control more configurable.
-
-        protected override void OnBindingContextChanged()
+        private void RenderSpanMarkdown()
         {
-            base.OnBindingContextChanged();
-
             if (BindingContext is SpanData data)
             {
                 Text = data.Text;
@@ -46,7 +42,7 @@ namespace Axvr.Xamarin.Markdown.Templates
 
                 if (data.Attributes.HasFlag(SpanAttributes.Link))
                 {
-                    TextColor = Color.FromHex("#0000EE");
+                    TextColor = LinkTextColor;
                     TextDecorations |= TextDecorations.Underline;
                 }
 
@@ -54,13 +50,95 @@ namespace Axvr.Xamarin.Markdown.Templates
                 {
                     Text = $"\u00A0{Text}\u00A0";
 
-                    ForegroundColor = Color.FromHex("#24292e");
-                    BackgroundColor = Color.FromHex("#f6f8fa");
-                    FontFamily = Device.RuntimePlatform == Device.iOS
-                               ? "Courier"
-                               : "monospace";
+                    ForegroundColor = InlineCodeForegroundColor;
+                    BackgroundColor = InlineCodeBackgroundColor;
+                    FontFamily = InlineCodeFontFamily;
                 }
             }
         }
+
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            RenderSpanMarkdown();
+        }
+
+
+        private static void OnSpanStylePropertiesChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is Span self && oldValue != newValue)
+            {
+                self.OnBindingContextChanged();
+            }
+        }
+
+
+        #region InlineCode
+
+        public static readonly BindableProperty InlineCodeForegroundColorProperty =
+            BindableProperty.Create(
+                propertyName: nameof(InlineCodeForegroundColor),
+                returnType: typeof(Color),
+                declaringType: typeof(Span),
+                defaultValue: Color.FromHex("#24292e"),
+                propertyChanged: OnSpanStylePropertiesChanged);
+
+        public Color InlineCodeForegroundColor
+        {
+            get => (Color)GetValue(InlineCodeForegroundColorProperty);
+            set => SetValue(InlineCodeForegroundColorProperty, value);
+        }
+
+
+        public static readonly BindableProperty InlineCodeBackgroundColorProperty =
+            BindableProperty.Create(
+                propertyName: nameof(InlineCodeBackgroundColor),
+                returnType: typeof(Color),
+                declaringType: typeof(Span),
+                defaultValue: Color.FromHex("#f6f8fa"),
+                propertyChanged: OnSpanStylePropertiesChanged);
+
+        public Color InlineCodeBackgroundColor
+        {
+            get => (Color)GetValue(InlineCodeBackgroundColorProperty);
+            set => SetValue(InlineCodeBackgroundColorProperty, value);
+        }
+
+
+        public static readonly BindableProperty InlineCodeFontFamilyProperty =
+            BindableProperty.Create(
+                propertyName: nameof(InlineCodeFontFamily),
+                returnType: typeof(string),
+                declaringType: typeof(Span),
+                defaultValue: Device.RuntimePlatform == Device.iOS ? "Courier" : "monospace",
+                propertyChanged: OnSpanStylePropertiesChanged);
+
+        public string InlineCodeFontFamily
+        {
+            get => (string)GetValue(InlineCodeFontFamilyProperty);
+            set => SetValue(InlineCodeFontFamilyProperty, value);
+        }
+
+        #endregion InlineCode
+
+
+        #region Link
+
+        public static readonly BindableProperty LinkTextColorProperty =
+            BindableProperty.Create(
+                propertyName: nameof(LinkTextColor),
+                returnType: typeof(Color),
+                declaringType: typeof(Span),
+                defaultValue: Color.FromHex("#0000EE"),
+                propertyChanged: OnSpanStylePropertiesChanged);
+
+        public Color LinkTextColor
+        {
+            get => (Color)GetValue(LinkTextColorProperty);
+            set => SetValue(LinkTextColorProperty, value);
+        }
+
+        #endregion Link
     }
 }
